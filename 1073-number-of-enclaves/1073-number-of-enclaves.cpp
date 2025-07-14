aed5cdcf560111ff0ff6b7de80dfb86d475f44e5
Bhavin Patel <116063613+BHAVINPATEL06@users.noreply.github.com>
2025-07-14 23:58:13 +0530

    Update 1073-number-of-enclaves.cpp

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-// Tere bhai ne khud se kiya hai
-// Bc dimag ka bhosda ho gya bc
+
 
     void solve(vector<vector<int>>&grid,int i,int j){
         if(i == grid.size() || j == grid[0].size() || i<0 || j<0)return;
@@ -9,12 +8,12 @@ public:
 
         grid[i][j] = 0;
 
-        // solve(grid,i+1,j);   // Pahele aise kar rha tha but isko maine change kiya toh ho gya
+        // solve(grid,i+1,j);  
         // solve(grid,i,j+1);
         // solve(grid,i-1,j);
         // solve(grid,i,j-1);
 
-        int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}}; // Ye bhi tere bhai ne khud se likha hai
+        int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}}; 
 
         for(int d = 0;d<4;d++){
             int newI = i+dirs[d][0];
@@ -30,7 +29,6 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int ans = 0;
         for(int i = 0;i<grid[0].size();i++){ // First Row // Correct
-            // cout << grid[0][i] << " ";
             if(grid[0][i] == 1){
                 solve(grid,0,i);
             }
@@ -38,14 +36,12 @@ public:
 
         int lastCol = grid[0].size()-1;
         for(int i = 0;i<grid.size();i++){ // Last Col // Correct
-            // cout << grid[i][lastCol] << " ";
             if(grid[i][lastCol] == 1){
                 solve(grid,i,lastCol);
             }
         }
 
         for(int i = 0;i<grid.size();i++){ // First Col // Correct
-            // cout << grid[i][0] << " ";
             if(grid[i][0] == 1){
                 solve(grid,i,0);
             }
@@ -54,7 +50,6 @@ public:
         int lastRow = grid.size()-1;
 
         for(int i = 0;i<grid[0].size();i++){ // Last Row // COrrect
-            // cout << grid[lastRow][i] << " ";
             if(grid[lastRow][i] == 1){
                 solve(grid,lastRow,i);
             }
@@ -63,9 +58,7 @@ public:
         for(int i = 0;i<grid.size();i++){
             for(int j = 0;j<grid[0].size();j++){
                 if(grid[i][j] == 1)ans++;
-                // cout << grid[i][j] << " ";
             }
-            // cout << endl;
         }
         return ans;
     }
