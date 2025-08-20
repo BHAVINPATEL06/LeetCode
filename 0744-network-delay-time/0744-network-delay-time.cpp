f62ab6bf9b3260a2406bda6de2b6f632155430d8
Bhavin Patel <116063613+BHAVINPATEL06@users.noreply.github.com>
2025-08-20 16:47:12 +0530

    Time: 76 ms (86.12%), Space: 44.9 MB (50.81%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
 
-    void solve(int src,unordered_map<int,vector<pair<int,int>>>&adjList,vector<int>&dist){
+    void solve(int src,int n,unordered_map<int,vector<pair<int,int>>>&adjList,vector<int>&dist){
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
         pq.push({0,src});
 
         while(!pq.empty()){
-            auto front = pq.top();
+            auto temp = pq.top();
             pq.pop();
-
-            int u = front[1];
+            int u = temp[1];
 
             for(auto nbr:adjList[u]){
                 int v = nbr.first;
                 int wt = nbr.second;
 
-                if(dist[v]>dist[u]+wt){
-                    dist[v] = dist[u]+wt;
+                if(dist[v]>wt+dist[u]){
+                    dist[v] = wt+dist[u];
                     pq.push({dist[v],v});
                 }
             }
@@ -32,19 +31,16 @@ public:
             int wt = times[i][2];
 
             adjList[u].push_back({v,wt});
-            // adjList[v].push_back({u,wt});
         }
 
         vector<int>dist(n+1,INT_MAX);
         dist[k] = 0;
-        solve(k,adjList,dist);
-        int ans = INT_MIN;
+        solve(k,n,adjList,dist);
 
+        int ans = INT_MIN;
         for(int i = 1;i<dist.size();i++){
             ans = max(ans,dist[i]);
-            cout << dist[i] << " ";
         }
-
         return ans == INT_MAX?-1:ans;
     }
 };
